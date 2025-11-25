817cd6de4f8f780bb672b505151755ae29107f20
Praneket <jadhavpraneket@gmail.com>
2025-11-25 22:16:40 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,25 @@
-require("dotenv").config();
+const path = require("path");
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
-
-const apodRoutes = require("./routes/apod");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // API routes
-app.use("/api/apod", apodRoutes);
+app.use("/api/apod", require("./routes/apod"));
 
 // Serve frontend
 const frontendPath = path.join(__dirname, "../frontend/dist");
+
 app.use(express.static(frontendPath));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
-// Start server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`APOD backend listening on ${port}`);
+  console.log(`Server running on port ${port}`);
 });
